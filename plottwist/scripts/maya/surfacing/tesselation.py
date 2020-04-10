7d5330f8ca716cf5e83f125ed8f37aec23e39ba4
Tomas Poveda <tpovedatd@gmail.com>
2020-04-10 20:30:37 +0200

    Updated tesselation to fit PEP8 standards

--- plottwist/scripts/maya/surfacing/tesselation.py
+++ plottwist/scripts/maya/surfacing/tesselation.py
@@ -15,22 +15,29 @@ __email__ = ""
 
 import maya.cmds as mc
 
-def Tesselate(selection=False):
 
-    if not selection: # Check Selection
-        selection = mc.ls(sl=True)       
+def tesselate(selection=False):
 
-    mc.filterExpand(ex =True, sm=12) # Extract only polymesh
-    if len(selection) == 1: # Check how many elements are present in the list
+    # Check Selection
+    if not selection:
+        selection = mc.ls(sl=True)
+
+    # Extract only polymesh
+    mc.filterExpand(ex=True, sm=12)
+
+    # Check how many elements are present in the list
+    if not selection:
+        raise Exception('You must select a mesh')
+
+    if len(selection) == 1:
         selection = [selection[0]]
-    elif len(selection) == False:
-        raise Exception ('You must select a mesh')
-    else:
-        pass
     
-    for i in selection :
-        mc.setAttr(i + '.aiSubdivType', 1) # Type : 0 = None, 1 = Catclark, 2 = Linear
-        mc.setAttr(i + '.aiSubdivIterations', 2) # Subdivsion Iterations
-        mc.setAttr(i + '.aiSubdivAdaptiveMetric', 1) # Adaptive Metric : 0 = Auto, 1 = Edge_length, 2 = Flatness
-       
-Tesselate()   
+    for i in selection:
+        # Type : 0 = None, 1 = Catclark, 2 = Linear
+        mc.setAttr(i + '.aiSubdivType', 1)
+
+        # Subdivsion Iterations
+        mc.setAttr(i + '.aiSubdivIterations', 2)
+
+        # Adaptive Metric : 0 = Auto, 1 = Edge_length, 2 = Flatness
+        mc.setAttr(i + '.aiSubdivAdaptiveMetric', 1)
