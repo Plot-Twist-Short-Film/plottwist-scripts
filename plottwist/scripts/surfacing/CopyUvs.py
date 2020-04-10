402164a6ac92b5932d94757f0409fd77bf8d2abe
Tomas Poveda <tpovedatd@gmail.com>
2020-04-10 13:37:48 +0200

    Updated CopyUvs.py script
    + Added standard header
    + Added code into a function
    + Updated code to fit PEP8 standards

--- plottwist/scripts/surfacing/CopyUvs.py
+++ plottwist/scripts/surfacing/CopyUvs.py
@@ -1,23 +1,36 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copy Uvs Component Mode for Multiple Meshes
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Titouan Perrot"
+__license__ = "MIT"
+__maintainer__ = "Titouan Perrot"
+__email__ = ""
+
+
 import maya.cmds as mc
 
-#Copy Uvs Component Mode for Multiple Meshes
 
-#grab all the selected objects
+def copy_uvs():
+    """
+    Copy Uvs Component Mode for Multiple Meshes
+    """
 
-selectedObjects = mc.ls(sl=True)
+    # grab all the selected objects
+    selected_objects = mc.ls(sl=True)
 
-#save first one into variable
+    # save first one into variable
+    # pop first one out of the selected objects list
+    driver = selected_objects.pop(0)
 
-#pop first one out of the selected objects list
+    # for each object in the selected objects list
+    for object in selected_objects:
+        mc.select([driver, object])
 
-driver = selectedObjects.pop(0)
-
-#for each object in the selected objects list
-
-for object in selectedObjects:
-
-    mc.select([driver,object])
-
-    #transfer attributes
-
-    mc.transferAttributes(sampleSpace=4,transferUVs=2, transferColors=0 )
+        #transfer attributes
+        mc.transferAttributes(sampleSpace=4,transferUVs=2, transferColors=0)
